80d6421c0cfeb5632ea65efc3793a0e1ce4a3c64
OhYeah229 <aaron.fuller@protonmail.com>
2019-03-31 22:07:28 -0400

    Done with Arrays and Function Conversion.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -109,22 +109,16 @@ console.log("Total cars under year 2000: " + oldCars.length);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 
-
+//inventory is an array of 50 objects. In those objects there are details about the car.
 let carList2 = [];
 let BMWAndAudi = [];
 
 for(let i = 0; i < inventory.length; i++){
     carList2.push({ car_make: inventory[i].car_make, car_model: inventory[i].car_model})
 }
-// now we have our objects in the array
-
-for(let i = 0; i < carList2.length; i++) {
-    if(carList2[i].car_make == "Audi" || carList2[i].car_make == "BMW") {
-        BMWAndAudi.push({car_make: carList2[i].car_make, car_model: carList2[i].car_model});
+    for(let i = 0; i < carList2.length; i++) {
+        if(carList2[i].car_make == "Audi" || carList2[i].car_make == "BMW") {
+            BMWAndAudi.push({car_make: carList2[i].car_make, car_model: carList2[i].car_model});
+        }
     }
-    //BMWAndAudi is now printing correct data, just need to console.log correctly
-}
 console.log(JSON.stringify(BMWAndAudi));
-
-
-
